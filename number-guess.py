a81ab06a25326c4b39f89ce0d0491b8f06a83993
Matt Bingham <mattbingham@outlook.com>
2017-04-20 12:49:34 +0200

    Working copy
    Needs further changes detailed in file

--- number-guess.py
+++ number-guess.py
@@ -4,10 +4,37 @@ __author__ = "Matt Bingham"
 __email__ = "mattbingham@outlook.com"
 
 """
-TO DO
-Generate random (full) number between 1 and 10
-User input for guess of number
-If correct, game ends
-If incorrect, user told that number is higher or lower than value input
-If tries exceeds 3, user loses
+TO-DO
+Assign to definitions
+Compensate for user input error
 """
+import random
+
+number = random.randint(1, 10)
+go = 1
+gos = 3
+decorate = "=" * 20
+print(decorate)
+print("Can you guess the number between 1 and 10?")
+print("Guess", go, "of", gos)
+guess = int(input("> "))
+
+while guess != number:
+	print(decorate)
+	print("Nope, that's not correct.")
+	print("Guess", go, "of", gos)
+	if gos == go:
+		print("To many wrong answers - better luck next time!")
+		break
+	else:
+		if guess > number:
+			print("You need to go lower.")
+			guess = int(input("> "))
+			go += 1
+		else:
+			print("You need to go higher.")
+			guess = int(input("> "))
+			go += 1
+if guess == number:
+	print(decorate)
+	print("Good guess! You win!")
