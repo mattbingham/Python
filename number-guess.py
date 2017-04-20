839add46d3b4925785327c14e663cd4cde1e5a1d
Matt Bingham <mattbingham@outlook.com>
2017-04-20 11:55:07 +0200

    Initial commit with to-do list
    Schematic for game

---
+++ number-guess.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Guess a number, returns higher or lower if incorrect"""
+__author__ = "Matt Bingham"
+__email__ = "mattbingham@outlook.com"
+
+"""
+TO DO
+Generate random (full) number between 1 and 10
+User input for guess of number
+If correct, game ends
+If incorrect, user told that number is higher or lower than value input
+If tries exceeds 3, user loses
+"""
