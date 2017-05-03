141746157949cd508d6eb0357d3dcbbe97fcea98
Matt Bingham <mattbingham@outlook.com>
2017-05-03 15:36:32 +0200

    Save commented section
    In case required later

--- web-scraper_headings.py
+++ web-scraper_headings.py
@@ -12,7 +12,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # Create a variable with the url
-url = input("Enter the URL you want to parse headings from: ")
+url = str(input("Enter the URL you want to parse headings from: "))
 decoration = "=" * 20
 
 # Use requests to get the contents
@@ -35,27 +35,80 @@ h5 = soup.find_all('h5')
 print(decoration)
 print("Heading for page:\n{}".format(soup.title.string))
 
-print(decoration)
-print("H1 headers:\n-----------")
-for h in h1:
-	print(h.get_text())
-print(decoration)
+h = str(input("""
+	Which title would you like to print? :
+	Press 1 for all h1 headings
+	Press 2 for all h2 headings
+	Press 3 for all h3 headings
+	Press 4 for all h4 headings
+	Press 5 for all h5 headings
+	Press 6 for all headings > \n
+	"""))
 
-print("H2 headers:\n-----------")
-for h in h2:
-	print(h.get_text())
-print(decoration)
+h = "h{}".format(h)
+print(h)
 
-print("H3 headers:\n-----------")
-for h in h3:
-	print(h.get_text())
-print(decoration)
+def headings(h):
+	# Get heading number input
 
-print("H4 headers:\n-----------")
-for h in h4:
-	print(h.get_text())
-print(decoration)
 
-print("H5 headers:\n-----------")
-for h in h5:
-	print(h.get_text())
+
+	# Loop and print out all input headers
+
+
+"""
+if get_titles == "1":
+	print("H1 headers:\n-----------")
+	for h in h1:
+		print(h.get_text())
+
+elif get_titles == "2":
+	print("H2 headers:\n-----------")
+	for h in h2:
+		print(h.get_text())
+
+elif get_titles == "3":
+	print("H3 headers:\n-----------")
+	for h in h3:
+		print(h.get_text())
+
+elif get_titles == "4":
+	print("H4 headers:\n-----------")
+	for h in h4:
+		print(h.get_text())
+
+elif get_titles == "5":
+	print("H5 headers:\n-----------")
+	for h in h5:
+		print(h.get_text())
+
+elif get_titles == "6":
+	print(decoration)
+	print("H1 headers:\n-----------")
+	for h in h1:
+		print(h.get_text())
+		print(decoration)
+
+	print("H2 headers:\n-----------")
+	for h in h2:
+		print(h.get_text())
+	print(decoration)
+
+	print("H3 headers:\n-----------")
+	for h in h3:
+		print(h.get_text())
+	print(decoration)
+
+	print("H4 headers:\n-----------")
+	for h in h4:
+		print(h.get_text())
+	print(decoration)
+
+	print("H5 headers:\n-----------")
+	for h in h5:
+		print(h.get_text())
+	# Get all headings
+	else:
+		tot = "You did not enter a valid function."
+
+"""
