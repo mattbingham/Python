35a4d4c3707f88bacf90dbdb5f99f372daeffa0c
Matt <mattbingham@users.noreply.github.com>
2017-07-11 13:02:20 +0200

    Improvements to output
    Added additional prints showing times a number thrown and total thrown.
    
    Needs more work to remove brackets and tidy code.

--- dice-roll.py
+++ dice-roll.py
@@ -3,23 +3,44 @@
 __author__ = "Matt Bingham"
 __email__ = "mattbingham@outlook.com"
 
-import random 
+import random
+from collections import Counter
 
 #Vars
-decorate = "=" * 20
+decorate = "=" * 10
+decorate1 = decorate + "="
 question = int(input("How many dice do you want to roll? > "))
 
 def dice_list(i):
 	lst = []
 	count = 1
+	total = 0
+	c1 = []
+	c2 = []
 	while i != 0:
 		roll = random.randint(1, 6)
 		lst.append(roll)
 		print("Roll {}: {}".format(count, lst[count-1]))
-		print(decorate)
+		if roll < 10:
+			print(decorate1)
+		else:
+			print(decorate)
 		i -= 1
 		count += 1
-	print("Your total rolls are", lst)
+	for n in lst:
+		total = total + n
+
+	variations = Counter(lst)
+	for n in variations:
+		c1.append(n)
+	print(decorate * 6)
+	print("STATS")
+	print("Your rolls list   :\t", lst)
+	print("Rolls total count :\t", total)
+	print("Unique numbers    :\t", c1)
+	print("Number consistancy:\t")
+	for k,v in variations.items():
+	    print(k,"x",v)
 
 #Ask
 print(question)
