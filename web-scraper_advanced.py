42ea7aac2f027ccce812c223c76aa45d6c6a491c
Matt <mattbingham@users.noreply.github.com>
2017-07-16 23:27:11 +0200

    Refactored
    Now using classes and defs but need error validation for string input

--- web-scraper_advanced.py
+++ web-scraper_advanced.py
@@ -35,25 +35,44 @@ Press 2 for headings
 Press 3 for paragraphs
 > """))
 
-if user == 1:
-	decore = "links"
-	a = soup.find_all('a')
-	for i in a:
-		print("\t", i.get_text)
-	print(a)
-elif user == 2:
-	decore = "headings"
-	h = input("{}\nPress 1 for h1 {}\nPress 2 for h2 {}\nPress 3 for h3 {}\nPress 4 for h4 {} > ".format(decoration, decore, decore, decore, decore))
-	print("{}\nPage title: \n\t{}".format(decoration, soup.title.string))
-	user = "h{}".format(h)
-	h = soup.find_all(user)
-	print(decoration, "\n{} {}:".format(user, decore))
-	for i in h:
-		print("\t", i.get_text())
-elif user == 3:
-	decore = "paragraphs"
-	n = int(input("How many paragraphs do you want to output? > "))
-	p = soup.find_all('p')[0:n]
-	print(decoration, p)
-else:
-	print("That input is not valid.")
+"""Section defines what is input input the Selection class"""
+def q(user):
+	if user == 1:
+		a.links(user)
+	elif user == 2:
+		a.headings(user)
+	elif user == 3:
+		a.paragraphs(user)
+	else:
+		print("That input is not valid.")
+
+class Selection(object):
+	"""Takes the user input and selection and outputs below"""
+
+	def links(self, user):
+		d = "links"
+		a = soup.find_all('a')
+		for i in a:
+			print("\t", i.get_text)
+		print(a)		
+
+	def headings(self, user):
+		d = "headings"
+		h = input("{}\nPress 1 for h1 {}\nPress 2 for h2 {}\nPress 3 for h3 {}\nPress 4 for h4 {} > ".format(decoration, d, d, d, d))
+		print("{}\nPage title: \n\t{}".format(decoration, soup.title.string))
+		user = "h{}".format(h)
+		h = soup.find_all(user)
+		print(decoration, "\n{} {}:".format(user, d))
+		for i in h:
+			print("\t", i.get_text())
+		
+	def paragraphs(self, user):
+		d = "paragraphs"
+		n = int(input("How many paragraphs do you want to output? > "))
+		p = soup.find_all('p')[0:n]
+		print(decoration, p)
+
+#Run the program
+print(user)
+a = Selection()
+q(user)
